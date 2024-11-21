7ce14464384e88bd6e720ed3d3967bfb11d7f3c5
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2024-11-20 21:21:31 -0300

    feat: create new todo

--- resources/js/pages/Home/index.tsx
+++ resources/js/pages/Home/index.tsx
@@ -1,9 +1,64 @@
+import { FormEvent, useState } from 'react';
+
 interface HomeProps {}
 
+interface Todo {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
+
 export default function Home({}: HomeProps) {
+  const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  function handleTodoInput(event: FormEvent<HTMLInputElement>) {
+    setNewTodo(event.currentTarget.value);
+  }
+
+  function handleNewTodo(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (newTodo.length === 0) return;
+
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: crypto.getRandomValues(new Uint32Array(1)).toString(),
+        title: newTodo,
+        isCompleted: false,
+      },
+    ]);
+
+    setNewTodo('');
+  }
+
   return (
     <div>
-      <h1>Hello Inertia.js!</h1>
+      <h1>To-do List</h1>
+
+      <form onSubmit={handleNewTodo}>
+        <input
+          type="text"
+          name="todo"
+          value={newTodo}
+          onInput={handleTodoInput}
+        />
+        <button type="submit">
+          Criar nova tarefa
+        </button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input type="checkbox" id={`todo.${todo.id}`}/>
+            <label htmlFor={`todo.${todo.id}`}>
+              {todo.title}
+            </label>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
