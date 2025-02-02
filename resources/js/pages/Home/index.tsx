52596b8c2edc55ff71aa5835b339c6609a4520c5
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2025-02-02 14:42:55 -0300

    feat: add basic styles to Home

--- resources/js/pages/Home/index.tsx
+++ resources/js/pages/Home/index.tsx
@@ -42,24 +42,36 @@ export default function Home({ todos }: HomeProps) {
   }
 
   return (
-    <div>
-      <h1>To-do List</h1>
+    <div className="p-4">
+      <h1 className="text-2xl font-black uppercase mb-2">
+        To-do List
+      </h1>
 
-      <form onSubmit={handleNewTodo}>
+      <form
+        className="flex mb-4"
+        onSubmit={handleNewTodo}
+      >
         <input
+          className="px-2 bg-gray-100 border-2 border-transparent focus:border-black border-r-0 rounded-l outline-none transition-colors"
           type="text"
           name="todo"
           value={newTodo}
           onInput={handleTodoInput}
         />
-        <button type="submit">
+        <button
+          className="bg-blue-600 hover:bg-blue-800 transition-colors py-2 px-4 rounded-r text-white"
+          type="submit"
+        >
           Criar nova tarefa
         </button>
       </form>
 
-      <ul>
+      <ul className="flex flex-col gap-y-2">
         {todos.map((todo) => (
-          <li key={todo.id}>
+          <li
+            className="flex items-center gap-x-1"
+            key={todo.id}
+          >
             <input
               type="checkbox"
               id={`todo.${todo.id}`}
